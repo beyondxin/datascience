1d37f23972ca990dc1f661ded29e5a4919df4d6b
beyondxin <34165156+beyondxin@users.noreply.github.com>
2018-09-25 17:06:11 +0800

    Update leida.py

--- leida.py
+++ leida.py
@@ -2,12 +2,14 @@
 %matplotlib inline
 import matplotlib.pyplot as plt
 import numpy as np
+plt.rcParams['font.sans-serif'] = ['SimHei'] #显示中文标签
+plt.rcParams['axes.unicode_minus'] = False #用来正常显示负号
 labels = ["L","R","F","M","C"]
 names = locals()
 for i in range(5):
     names['cluster%s' %i] = data_zscored[data_zscored["y_hat"] == i].mean(axis = 0)
 
-fig =plt.figure()
+fig =plt.figure(figsize = (16,8))
 ax = fig.add_subplot(111, polar=True)
 for i in range(5):
     da = names['cluster%s' %i]
@@ -15,10 +17,11 @@ for i in range(5):
     data = np.concatenate((da[:-1],[da[0]])) # 闭合
     angles = np.concatenate((angles, [angles[0]])) # 闭合
     
-    ax.plot(angles,data)
+    ax.plot(angles,data,label = '客户群%s' %i)
     #ax.fill(angles, data, facecolor='r', alpha=0.25)# 填充
     ax.set_thetagrids(angles * 180/np.pi, labels, fontproperties="SimHei")
     ax.set_title("matplotlib雷达图", va='bottom', fontproperties="SimHei")
-    ax.set_rlim(0,10)
+    #ax.set_rlim(0,2)
+    ax.legend()
     ax.grid(True)
 plt.show()
